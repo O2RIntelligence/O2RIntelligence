5eedcc1fdc47f37cc860f17f608170f337a998eb
GSM Masum <masumgsm114@gmail.com>
2022-04-25 12:40:25 +0600

    Error Loading Adtelligent API endpoint--solved

--- public/js/adtelligent.js
+++ public/js/adtelligent.js
@@ -5,11 +5,11 @@ class adtelligent {
 
     async request(params, route = '', base = '') {
         var url = (base.length > 0 ? base : window["ADTELLIGENT_BASE_URL"]);
-        route = route.length > 0 ? "/" + route : "";
+        route = route.length > 0 ? "https://ssp.adtelligent.com/api/statistics/ssp2/" + route : "https://ssp.adtelligent.com/api/statistics/ssp2";
         return new Promise((resolve, reject) => {
             $.ajax({
                 type: 'GET',
-                url: url + route,
+                url: route,
                 headers: {
                     "x-authentication-session-id": this.seat.api_token
                 },
