564672f0824a6800b3544d0e96e767efad0c9ae3
GSM Masum <masumgsm114@gmail.com>
2022-06-20 22:44:34 +0600

    O2R-96-- Updated MasterAccount<==>SubAccount Relations

--- app/Model/GoogleAds/SubAccount.php
+++ app/Model/GoogleAds/SubAccount.php
@@ -8,13 +8,19 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class SubAccount extends Model
 {
-    use HasFactory,  SoftDeletes, UsesUuid;
+    use SoftDeletes, UsesUuid;
     protected $fillable=[
         'id',
         'master_account_id',
         'name',
         'account_id',
+        'timezone',
+        'currency_code',
         'is_active',
         'is_online',
     ];
+
+    public function masterAccount(){
+        $this->belongsTo('sub_accounts','master_account_id');
+    }
 }
