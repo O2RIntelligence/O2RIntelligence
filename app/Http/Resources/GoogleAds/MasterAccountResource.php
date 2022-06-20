27d2ad915d179f2b20ec2499ef12d1dbfb1eea14
GSM Masum <masumgsm114@gmail.com>
2022-06-20 15:26:19 +0600

    O2R-72-- Master Account Settings--Model,Resource

--- app/Http/Resources/GoogleAds/MasterAccountResource.php
+++ app/Http/Resources/GoogleAds/MasterAccountResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\GoogleAds;
 
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class MasterAccountResource extends JsonResource
@@ -9,11 +10,20 @@ class MasterAccountResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  Request  $request
      * @return array
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            "id"=>$this->id,
+            "name"=>$this->name,
+            "account_id"=>$this->account_id,
+            "developer_token"=>$this->developer_token,
+            "discount"=>$this->discount,
+            "revenue_conversion_rate"=>$this->revenue_conversion_rate,
+            "is_active"=>$this->is_active,
+            "is_online"=>$this->is_online,
+        ];
     }
 }
--- app/Model/GoogleAds/MasterAccount.php
+++ app/Model/GoogleAds/MasterAccount.php
@@ -3,13 +3,12 @@
 namespace App\Model\GoogleAds;
 
 use App\Traits\UsesUuid;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class MasterAccount extends Model
 {
-    use HasFactory,  SoftDeletes, UsesUuid;
+    use SoftDeletes, UsesUuid;
     protected $fillable=[
         'id',
         'name',
