061551dc6fbb59c48b4c834532dcfebac0ae69b5
GSM Masum <masumgsm114@gmail.com>
2022-06-20 15:27:07 +0600

    O2R-72-- Master Account Settings--Added Configs

---
+++ config/googleAds.php
@@ -0,0 +1,10 @@
+<?php
+
+//Google Ads related configs
+return[
+
+  "client_id"=> '427262788972-qrdrnh2tqk4tdh0irnshnu6p1l6emcfb.apps.googleusercontent.com',
+  "client_secret"=> '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',
+  "redirect_uri"=> 'http://localhost/o2r/googleAds/callback',
+
+];
