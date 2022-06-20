5a40261f54185d1f458a8f9f61d05b3cf09a4e1f
GSM Masum <masumgsm114@gmail.com>
2022-06-20 22:42:58 +0600

    O2R-96-- Added sub account discovery and active status

--- app/Http/Controllers/GoogleAds/SubAccountController.php
+++ app/Http/Controllers/GoogleAds/SubAccountController.php
@@ -3,84 +3,117 @@
 namespace App\Http\Controllers\GoogleAds;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\GoogleAds\MasterAccountResource;
+use App\Http\Resources\GoogleAds\SubAccountResource;
+use App\Model\GoogleAds\MasterAccount;
 use App\Model\GoogleAds\SubAccount;
+use App\Services\GoogleAds\AuthService;
+use App\Services\GoogleAds\GoogleAdsService;
+use Exception;
+use Google\Ads\GoogleAds\Lib\V10\GoogleAdsClient;
+use Google\Ads\GoogleAds\V10\Resources\Customer;
+use Google\ApiCore\ApiException;
+use Google\ApiCore\ValidationException;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class SubAccountController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
+    public function getGoogleAdsService(){
+        return new GoogleAdsService();
+    }
+    public function getGoogleAdsAuthService(){
+        return new AuthService();
     }
 
     /**
-     * Show the form for creating a new resource.
+     * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return JsonResponse|void
      */
-    public function create()
+    public function showAll()
     {
-        //
+        try {
+            if($subAccounts = SubAccountResource::collection(SubAccount::all())) {
+                return response()->json(['success' => true, 'data' => $subAccounts]);
+            }
+        } catch (Exception $exception) {
+            dd($exception);
+        }
     }
+
+    /**
+     * Gets all the sub-accounts of all the master accounts
+     *
+     * @return JsonResponse
+     */
+    public function getSubAccountsFromGoogleAds()
+    {
+        try {
+            $masterAccounts =  MasterAccountResource::collection(MasterAccount::all());
+            $subAccounts = [];
+            foreach ($masterAccounts as $key => $masterAccount) {
+                $googleAdsClient = $this->getGoogleAdsAuthService()->getGoogleAdsService($masterAccount);
+                $subAccounts[] = $this->getGoogleAdsService()->getAccountTree($masterAccount,$googleAdsClient);
+                $subAccounts = array_merge(...$subAccounts);
+                foreach ($subAccounts as $subAccount){
+                    if(!$subAccount['manager'] && $subAccount['level'] > 0) {
+                        $data['master_account_id'] = $masterAccount->id;
+                        $data['name'] = $subAccount['descriptiveName'];
+                        $data['account_id'] = $subAccount['id'];
+                        $data['timezone'] = $subAccount['timeZone'];
+                        $data['currency_code'] = $subAccount['currencyCode'];
+                        $data['is_active'] = true;
+                        $data['is_online'] = true;
+                        $this->store($data);
+                    }
+                }
+            }
+
+            return response()->json(['success'=>true]);
+        } catch (ApiException|ValidationException|Exception $exception) {
+            dd($exception);
+        }
+
+    }
+
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param $data
+     * @return JsonResponse
      */
-    public function store(Request $request)
+    private function store($data)
     {
-        //
+        try {
+            $subAccount = SubAccount::create([
+                'master_account_id' => $data['master_account_id'],
+                'name' => $data['name'],
+                'account_id' => $data['account_id'],
+                'timezone' => $data['timezone'],
+                'currency_code' => $data['currency_code'],
+                'is_active' => $data['is_active'],
+                'is_online' => $data['is_online']]);
+            return $subAccount->id;
+        } catch (Exception $exception) {
+            dd($exception);
+        }
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Model\GoogleAds\SubAccount  $subAccount
-     * @return \Illuminate\Http\Response
-     */
-    public function show(SubAccount $subAccount)
-    {
-        //
-    }
+  public function totalCost(){
+      try {
+          $subAccounts = SubAccount::with('masterAccount')->get()->all(); dd($subAccounts);
+          $dateRange['startDate'] = date('Y-m-01');
+          $dateRange['endDate'] = date('Y-m-d');
+          $cost = [];
+          foreach ($subAccounts as $subAccount) {
+              $cost [] = $this->getGoogleAdsService()->getTotalCost($subAccount, $dateRange);
+          }
+          return response()->json(['success' => true, 'data' => $cost]);
+      } catch (Exception $exception) {
+          dd($exception);
+      }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Model\GoogleAds\SubAccount  $subAccount
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(SubAccount $subAccount)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Model\GoogleAds\SubAccount  $subAccount
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, SubAccount $subAccount)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Model\GoogleAds\SubAccount  $subAccount
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(SubAccount $subAccount)
-    {
-        //
-    }
+  }
 }
---
+++ app/Http/Resources/GoogleAds/SubAccountResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources\GoogleAds;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class SubAccountResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            "id"=>$this->id,
+            "master_account_id"=>$this->master_account_id,
+            "name"=>$this->name,
+            "account_id"=>$this->account_id,
+            "is_active"=>$this->is_active,
+            "is_online"=>$this->is_online,
+        ];
+    }
+}
