c12666986cf2dc7ec2e621c1999b5c0d2e87b2ff
GSM Masum <masumgsm114@gmail.com>
2022-06-20 22:40:41 +0600

    O2R-96-- Added Google authentication Services

--- app/Services/GoogleAds/AuthService.php
+++ app/Services/GoogleAds/AuthService.php
@@ -3,20 +3,38 @@
 namespace App\Services\GoogleAds;
 
 
+use App\Model\GoogleAds\GoogleGrantToken;
+use Exception;
+use Google\Ads\GoogleAds\Lib\OAuth2TokenBuilder;
+use Google\Ads\GoogleAds\Lib\V10\GoogleAdsClient;
+use Google\Ads\GoogleAds\Lib\V10\GoogleAdsClientBuilder;
+use Google\Client;
+
 class AuthService
 {
 
+    private $clientID;
+    private $clientSecret;
+    private $redirectUri;
+
+    public function __construct()
+    {
+        $this->clientID = config('googleAds.client_id');
+        $this->clientSecret = config('googleAds.client_secret');
+        $this->redirectUri = config('googleAds.redirect_uri');
+    }
+
+
     /**
      * get google client object
-     * @return Google_Client
      */
-    public function getClient(): Google_Client
+    public function getClient()
     {
-        $client = new Google_Client();
-        $client->setApplicationName('Gmail API for CRM');
-        $client->setScopes('GoogleAdsScopes');
-        $client->setClientId($this->appId);
-        $client->setClientSecret($this->appSecret);
+        $client = new Client();
+        $client->setApplicationName('Google Ads O2R');
+        $client->setScopes('https://www.googleapis.com/auth/adwords');
+        $client->setClientId($this->clientID);
+        $client->setClientSecret($this->clientSecret);
         $client->setAccessType('offline');
         $client->setPrompt('select_account consent');
         $client->setRedirectUri(url($this->redirectUri));
@@ -26,26 +44,49 @@ class AuthService
     /**
      * get service
      * @param $email
-     * @return Google_Service_Gmail
      * @throws Exception
      */
-    public function getGoogleAdsService()
+    public function getGmailService($email)
+    {
+        if($googleUser = GoogleUser::query()->where('email',$email)->first()) {
+            $token = $googleUser->auth_token;
+
+            $client = $this->getClient();
+            $client->setAccessToken($token);
+            if ($client->isAccessTokenExpired()) {
+                $token = $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+                if (!$googleUser->update(['auth_token' => json_encode($token)])) {
+                    throw new Exception('Could not Update Token with refresh token expired');
+                }
+                $client->setAccessToken($token);
+            }//else token is not expired
+        }else{
+            throw new Exception('Could not find a Google user');
+        }
+        return new Google_Service_Gmail($client);
+    }
+
+    /**
+     * get google ads service
+     * @throws Exception
+     */
+    public function getGoogleAdsService($masterAccount)
     {
         try {
+            $tokens = GoogleGrantToken::get()->first();
             $oAuth2Credential = (new OAuth2TokenBuilder())
-                ->withClientId('')
-                ->withClientSecret('')
-                ->withRefreshToken('')
+                ->withClientId($this->clientID)
+                ->withClientSecret( $this->clientSecret)
+                ->withRefreshToken($tokens->refresh_token)
                 ->build();
 
-            $googleAdsClient = (new GoogleAdsClientBuilder())
+            return (new GoogleAdsClientBuilder())
                 ->withOAuth2Credential($oAuth2Credential)
-                ->withDeveloperToken('')
-                ->withLoginCustomerId('6432926678')
+                ->withDeveloperToken($masterAccount->developer_token)
+                ->withLoginCustomerId($masterAccount->account_id)
                 ->build();
-            return $googleAdsClient;
-        } catch (Exception $e) {
-
+        } catch (Exception $exception) {
+            return  $exception;
         }
     }
 }
