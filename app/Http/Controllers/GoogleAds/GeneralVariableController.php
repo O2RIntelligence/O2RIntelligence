46e59826cd855ae05340c308fd6005544fa16bf9
GSM Masum <masumgsm114@gmail.com>
2022-06-21 13:08:31 +0600

    O2R-74--General Variables CRUD

--- app/Http/Controllers/GoogleAds/GeneralVariableController.php
+++ app/Http/Controllers/GoogleAds/GeneralVariableController.php
@@ -3,84 +3,106 @@
 namespace App\Http\Controllers\GoogleAds;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreGeneralVariableRequest;
+use App\Http\Requests\UpdateGeneralVariableRequest;
+use App\Http\Resources\GoogleAds\GeneralVariableResource;
 use App\Model\GoogleAds\GeneralVariable;
+use Exception;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class GeneralVariableController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
+    /**Stores a General Variable
+     * @param StoreGeneralVariableRequest $request
+     * @return JsonResponse
      */
-    public function index()
+    public function store(StoreGeneralVariableRequest $request)
     {
-        //
+        try {
+            $generalVariables = $request->validated();
+            if(GeneralVariable::create($generalVariables)){
+                return response()->json(array(['success' => true]));
+            }else return response()->json(array(['success' => false]));
+        } catch (Exception $exception) {
+            dd($exception);
+        }
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
+    /** Shows a specific General Variable from provided General Variable id
+     * @param UpdateGeneralVariableRequest $request
+     * @return JsonResponse|void
      */
-    public function create()
+    public function show(UpdateGeneralVariableRequest $request)
     {
-        //
+        try {
+            if($generalVariables = GeneralVariable::where('id', $request->id)->get()->first()) {
+                return response()->json(['success' => true, 'data' => new GeneralVariableResource($generalVariables)]);
+            }else return response()->json(['success' => false, 'message' => 'No Data Found']);
+        } catch (Exception $exception) {
+            dd($exception);
+        }
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+    /**Shows All General Variables
+     * @return JsonResponse
      */
-    public function store(Request $request)
+    public function getAll()
     {
-        //
+        try {
+            if($generalVariables = GeneralVariableResource::collection(GeneralVariable::all())) {
+                return response()->json(array(['success' => true, 'data' => $generalVariables]));
+            }
+        } catch (Exception $exception) {
+            dd($exception);
+        }
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Model\GoogleAds\GeneralVariable  $generalVariable
-     * @return \Illuminate\Http\Response
+    /** Updates a General Variable on provided id
+     * @param UpdateGeneralVariableRequest $request
+     * @return JsonResponse|void
      */
-    public function show(GeneralVariable $generalVariable)
+    public function update(UpdateGeneralVariableRequest $request)
     {
-        //
+        try {
+            $generalVariables = GeneralVariable::where('id', $request->id)->get()->first();
+            if ($generalVariables&&$generalVariables->update($request->validated())) {
+                return response()->json(array(['success' => true]));
+            }else return response()->json(['success' => false, 'message' => 'No Data Found']);
+        } catch (Exception $exception) {
+            dd($exception);
+        }
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Model\GoogleAds\GeneralVariable  $generalVariable
-     * @return \Illuminate\Http\Response
+    /**Changes Status of a General Variable
+     * @param UpdateGeneralVariableRequest $request
+     * @return JsonResponse|void
      */
-    public function edit(GeneralVariable $generalVariable)
+    public function switchStatus(UpdateGeneralVariableRequest $request)
     {
-        //
+        try {
+            $generalVariables = GeneralVariable::where('id', $request->id)->get()->first();
+            if ($generalVariables&&$generalVariables->update([$generalVariables->is_active = !$generalVariables->is_active])) {
+                return response()->json(array(['success' => true]));
+            }else return response()->json(['success' => false, 'message' => 'No Data Found']);
+        } catch (Exception $exception) {
+            dd($exception);
+        }
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Model\GoogleAds\GeneralVariable  $generalVariable
-     * @return \Illuminate\Http\Response
+    /**Deletes a General Variable
+     * @param UpdateGeneralVariableRequest $request
+     * @return JsonResponse
      */
-    public function update(Request $request, GeneralVariable $generalVariable)
+    public function delete(UpdateGeneralVariableRequest $request)
     {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Model\GoogleAds\GeneralVariable  $generalVariable
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(GeneralVariable $generalVariable)
-    {
-        //
+        try {
+            if(GeneralVariable::withTrashed()->where('id', $request->id)->delete()) {
+                return response()->json(array(['success' => true]));
+            }
+        } catch (Exception $exception) {
+            dd($exception);
+        }
     }
 }
