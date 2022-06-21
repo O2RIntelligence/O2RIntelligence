a7012d206ccef7bbd2e139e4e3bfd3d480db4f27
GSM Masum <masumgsm114@gmail.com>
2022-06-21 20:21:02 +0600

    O2R-70--Added Date Range Data insertion to db--added formula unnamed constants

--- config/googleAds.php
+++ config/googleAds.php
@@ -6,5 +6,9 @@ return[
   "client_id"=> '427262788972-qrdrnh2tqk4tdh0irnshnu6p1l6emcfb.apps.googleusercontent.com',
   "client_secret"=> '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',
   "redirect_uri"=> 'http://localhost/o2r/googleAds/callback',
+    //constants
+  "micro_cost"=> 1000000,
+  "rapid_api_key"=> '6a8ca8f094msh01391916ebee8c9p1d0a16jsndb16ed1be613',
+  "google_media_cost_constant"=> 0.21,
 
 ];
