8ad634efe3a5cdc18cffdeddf773578c73ea2969
GSM Masum <masumgsm114@gmail.com>
2022-06-22 20:04:36 +0600

    O2R-56--Added Monthly Projection data, O2R-54--Refactored Daily Projection Data

--- app/Http/Controllers/GoogleAds/DashboardController.php
+++ app/Http/Controllers/GoogleAds/DashboardController.php
@@ -20,14 +20,14 @@ class DashboardController extends Controller
             $startDate = date('Y-m-d', strtotime($request->startDate??'today'));
             $endDate = date('Y-m-d', strtotime($request->endDate??'today'));
             $accountInformation = $this->getAccountInformation();
-            $dailyProjection = $this->getDailyProjectionData($startDate, $accountInformation['masterAccounts'], $accountInformation['subAccounts']);
+            $dailyProjection = $this->getDailyCostRunRateData($startDate, $accountInformation['masterAccounts'], $accountInformation['subAccounts']);
             $monthlyProjection = $this->getMonthlyCostAndRunRateData($startDate,$endDate, $accountInformation['masterAccounts'], $accountInformation['subAccounts']);
 //            $hourlyProjection = $this->getHourlyProjectionData($startDate, $endDate);
 
             return response()->json([
-//                'accountInformation' => $accountInformation,
+                'accountInformation' => $accountInformation,
                 'dailyProjection' => $dailyProjection,
-//                'monthlyProjection' => $monthlyProjection,
+                'monthlyProjection' => $monthlyProjection,
 //                'hourlyProjection' => $hourlyProjection,
             ]);
         } catch (Exception $exception) {
@@ -41,18 +41,6 @@ class DashboardController extends Controller
             $masterAccounts = MasterAccountResource::collection(MasterAccount::where('is_active',true)->where('is_online',true)->get());
             $subAccounts = SubAccountResource::collection(SubAccount::where('is_active',true)->where('is_online',true)->get());
             return array('masterAccounts'=>$masterAccounts,'subAccounts'=>$subAccounts);
-        } catch (Exception $exception) {
-            dd($exception);
-        }
-    }
-
-
-    public function getDailyProjectionData($startDate, $masterAccounts,$subAccounts)
-    {
-        try {
-            $dailyCost = $this->getDailyCostData($startDate,$masterAccounts,$subAccounts);
-            $dailyRunRate = $this->getDailyRunRateData($startDate,$masterAccounts,$subAccounts);
-            return array('dailyCost' => $dailyCost, 'dailyRunRate' => $dailyRunRate);
         } catch (Exception $exception) {
             dd($exception);
         }
@@ -108,46 +96,11 @@ class DashboardController extends Controller
         }
     }
 
-    public function getDailyCostData($date,$masterAccounts,$subAccounts) //Sum of Total Cost Today/Till Today
+    public function getDailyCostRunRateData($date,$masterAccounts,$subAccounts) //Sum of Total Cost Today/Till Today
     {
         try {
             $dayCount = date('d', strtotime($date));
             $totalDailyCost = 0;
-            $masterAccountData = [];
-            $subAccountData = [];
-            foreach($masterAccounts as $key1=>$masterAccount){
-                $masterAccountData[$key1]['id'] = $masterAccount->id;
-                $masterAccountData[$key1]['account_id'] = $masterAccount->account_id;
-                $masterAccountData[$key1]['name'] = $masterAccount->name;
-
-                foreach($subAccounts as $key2=>$subAccount){
-                    if ($subAccount->master_account_id == $masterAccount->id) {
-                        $subAccountData[$key2]['id'] = $subAccount->id;
-                        $subAccountData[$key2]['account_id'] = $subAccount->account_id;
-                        $subAccountData[$key2]['name'] = $subAccount->name;
-
-                        if (date('Y-m-d') == $date) {
-                            $dailyCost = HourlyData::where('sub_account_id', $subAccount->id)->sum('cost') / $dayCount;
-                        } else {
-                            $dataFromDate = DailyData::where('sub_account_id', $subAccount->id)->where('date', $date)->sum('cost');
-                            $dailyCost = $dataFromDate / $dayCount;
-                        }
-                        $totalDailyCost+=$dailyCost;
-                        $subAccountData[$key2]['cost'] = empty($subAccountData[$key2]['cost'])?$dailyCost:$subAccountData[$key2]['cost']+$dailyCost;
-                    }
-                }
-                $masterAccountData[$key1]['cost'] = empty( $masterAccountData[$key1]['cost'])?$totalDailyCost: $masterAccountData[$key1]['cost']+$totalDailyCost;
-
-            }
-            return array('totalDailyCost'=>$totalDailyCost, 'masterAccountData'=>$masterAccountData ,'subAccountData'=>$subAccountData);
-        } catch (Exception $exception) {
-            dd($exception);
-        }
-    }
-
-    public function getDailyRunRateData($date,$masterAccounts,$subAccounts) //Today Spent/ today Hours * 24
-    {
-        try {
             $totalDailyRunRate = 0;
             $masterAccountData = [];
             $subAccountData = [];
@@ -162,25 +115,30 @@ class DashboardController extends Controller
                         $subAccountData[$key2]['account_id'] = $subAccount->account_id;
                         $subAccountData[$key2]['name'] = $subAccount->name;
 
-                        if(date('Y-m-d') == $date) {
-                            $dailyRunRate = (HourlyData::where('sub_account_id', $subAccount->id)->sum('cost')/HourlyData::count())*24;
-                        }else{
+                        if (date('Y-m-d') == $date) {
+                            $hourlyData = HourlyData::where('sub_account_id', $subAccount->id)->sum('cost');
+                            $dailyCost =  $hourlyData/ $dayCount;
+                            $dailyRunRate = ($hourlyData/HourlyData::count())*24;
+                        } else {
                             $dataFromDate = DailyData::where('sub_account_id', $subAccount->id)->where('date', $date)->sum('cost');
+                            $dailyCost = $dataFromDate / $dayCount;
                             $dailyRunRate = $dataFromDate;
                         }
+                        $totalDailyCost+=$dailyCost;
+                        $subAccountData[$key2]['cost'] = empty($subAccountData[$key2]['cost'])?$dailyCost:$subAccountData[$key2]['cost']+$dailyCost;
                         $totalDailyRunRate+=$dailyRunRate;
                         $subAccountData[$key2]['runRate'] = empty($subAccountData[$key2]['runRate'])?$dailyRunRate:$subAccountData[$key2]['runRate']+$dailyRunRate;
                     }
                 }
+                $masterAccountData[$key1]['cost'] = empty( $masterAccountData[$key1]['cost'])?$totalDailyCost: $masterAccountData[$key1]['cost']+$totalDailyCost;
                 $masterAccountData[$key1]['runRate'] = empty( $masterAccountData[$key1]['runRate'])?$totalDailyRunRate: $masterAccountData[$key1]['runRate']+$totalDailyRunRate;
-
             }
-
-            return array('totalDailyCost'=>$totalDailyRunRate, 'masterAccountData'=>$masterAccountData ,'subAccountData'=>$subAccountData);
+            return array('totalDailyCost'=>$totalDailyCost,'totalDailyRunRate'=>$totalDailyRunRate, 'masterAccountData'=>$masterAccountData ,'subAccountData'=>$subAccountData);
         } catch (Exception $exception) {
             dd($exception);
         }
     }
+
 
 
 }
