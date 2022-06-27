08060e32c4851d3e876e4952a1183678863f2925
saeed-bin <saeed@blubirdinteractive.com>
2022-06-27 17:23:03 +0600

    O2R-104 common filtering and account selection desig added on overall page in googleAds module - Saeed

--- resources/views/googleAds/common_script.blade.php
+++ resources/views/googleAds/common_script.blade.php
@@ -2,4 +2,8 @@
     $("#account-filter").select2({
         placeholder: 'Select'
     })
+
+    $(document).on("click", "#show-daterange", function(){
+        $("#date-range-area").slideToggle('slow');
+    })
 </script>
--- resources/views/googleAds/dashboard.blade.php
+++ resources/views/googleAds/dashboard.blade.php
@@ -46,33 +46,60 @@
                             </div>
                         </div>
 
-                        <div class="box-body" style="padding-left: 0">
-                             <div class="row">
-                                <div class="col-md-6">
-
+                        
+                        <div class="box grid-box with-border"> 
+                            <div class="box-body">
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        Income Channel Overview
+                                    </div>
+                                    <div class="col-md-7 text-right">
+                                        <select name="account-filter" id="account-filter" class="form-control" multiple>
+                                            <option value="1">Account one</option>
+                                            <option value="2">Account two</option>
+                                            <option value="3">Account three</option>
+                                        </select>
+                                    </div>
+                                    <div class="col-md-1 text-right">
+                                        <button class="refresh-seats btn-black btn btn-block" style="max-height: 32px">Refresh</button>
+                                    </div>
+                                 </div>
+                                 <div class="row" style="border-top: 1px solid #f4f4f4; padding-top: 10px; margin-top: 10px">
+                                    <div class="col-md-4">
+                                        Generic report date scope:    
+                                    </div>
+                                    <div class="col-md-8 text-right">
+                                        <div class="pull-right time-periods">
+                                            <input type="hidden" name="date_period" value="today">
+                                            <button data-period="today" class="btn btn-secondary change-period active" type="button">Today</button>
+                                            <button data-period="yesterday" class="btn btn-secondary change-period" type="button">Yesterday</button>
+                                            <button data-period="last7" class="btn btn-secondary change-period" type="button">Last 7 Days</button>
+                                            <button data-period="last30" class="btn btn-secondary change-period" type="button">This Month</button>
+                                            <button data-period="custom" class="btn btn-secondary change-period" id="show-daterange" type="button">
+                                                Custom Range
+                                                <span class="caret"></span>
+                                            </button> 
+                                        </div> 
+                                    </div> 
+                                 </div>
+                                 <div class="date-range-area" id="date-range-area">
+                                    <div>
+                                         <div class="col-md-5 left">
+                                            <input type="text" class="form-control" name="start_date" id="start_date"> 
+                                         </div>
+                                         <div class="col-md-1">
+                                            to
+                                         </div>
+                                         <div class="col-md-5 right"> 
+                                            <input type="text" class="form-control" name="send_date" id="end_date">
+                                         </div>
+                                         <div class="col-md-2"> 
+                                            <button type="button" class="filter-dates btn btn-block btn-primary">Filter</button>
+                                         </div>
+                                    </div>
                                 </div>
-                                <div class="col-md-6 text-right">
-                                    
-                                </div>
-                             </div>
-                        </div>
-                        <div class="box-body" style="padding-left: 0">
-                             <div class="row">
-                                <div class="col-md-4">
-                                    Income Channel Overview
-                                </div>
-                                <div class="col-md-7 text-right">
-                                    <select name="account-filter" id="account-filter" class="form-control" multiple>
-                                        <option value="1">Account one</option>
-                                        <option value="2">Account two</option>
-                                        <option value="3">Account three</option>
-                                    </select>
-                                </div>
-                                <div class="col-md-1 text-right">
-                                    <button class="refresh-seats btn-black btn btn-sm btn-block" style="margin-top: 2px;">Refresh</button>
-                                </div>
-                             </div>
-                        </div>
+                            </div>
+                        </div>  
                     </div>
                 </div>
 
