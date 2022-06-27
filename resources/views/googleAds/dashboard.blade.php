d592b49cf5635aa452b9453d42f1a8e937fb0239
saeed-bin <saeed@blubirdinteractive.com>
2022-06-27 18:45:19 +0600

    O2R-104 daterange picker added in common filter section in dashboard - Saeed

--- resources/views/googleAds/dashboard.blade.php
+++ resources/views/googleAds/dashboard.blade.php
@@ -31,29 +31,41 @@
                 <div class="row">
                     <div class="col-md-12">
                         <div class="box-body" style="padding-left: 0">
-                            <div class="pull-left">
-                                <div class="btn-group" data-toggle="buttons">
-                                    <label class="btn btn-info active">
-                                        <input name="page_type" type="radio" value="general" checked=""> General
-                                    </label>
-                                    <label class="btn btn-info">
-                                        <input name="page_type" type="radio" value="muster-account"> Muster Account
-                                    </label>
-                                    <label class="btn btn-info">
-                                        <input name="page_type" type="radio" value="sub-account"> Sub Account
-                                    </label>
+                            <div class="row">
+                                <div class="col-md-5">
+                                    <div class="btn-group" data-toggle="buttons">
+                                        <label class="btn btn-info active">
+                                            <input name="page_type" type="radio" value="general" checked=""> General
+                                        </label>
+                                        <label class="btn btn-info">
+                                            <input name="page_type" type="radio" value="muster-account"> Muster Account
+                                        </label>
+                                        <label class="btn btn-info">
+                                            <input name="page_type" type="radio" value="sub-account"> Sub Account
+                                        </label>
+                                    </div>
                                 </div>
-                            </div>
+                                <div class="col-md-7 text-right">
+                                    <div class="pull-right time-periods">
+                                        <input type="hidden" name="date_period" value="today">
+                                        <button data-period="today" class="btn btn-secondary change-period active" type="button">Today</button>
+                                        <button data-period="yesterday" class="btn btn-secondary change-period" type="button">Yesterday</button>
+                                        <button data-period="last7" class="btn btn-secondary change-period" type="button">Last 7 Days</button>
+                                        <button data-period="last30" class="btn btn-secondary change-period" type="button">This Month</button>
+                                        <button data-period="custom" class="btn btn-secondary change-period" id="daterange-btn" type="button">
+                                            <i class="fa fa-calendar"></i> Custom Range
+                                            <i class="fa fa-caret-down"></i>
+                                        </button>  
+                                    </div>
+                                </div>
+                            </div> 
                         </div>
 
                         
                         <div class="box grid-box with-border"> 
-                            <div class="box-body">
-                                <div class="row">
-                                    <div class="col-md-4">
-                                        Income Channel Overview
-                                    </div>
-                                    <div class="col-md-7 text-right">
+                            <div class="box-body">  
+                                <div class="row"> 
+                                    <div class="col-md-11 text-right">
                                         <select name="account-filter" id="account-filter" class="form-control" multiple>
                                             <option value="1">Account one</option>
                                             <option value="2">Account two</option>
@@ -63,41 +75,7 @@
                                     <div class="col-md-1 text-right">
                                         <button class="refresh-seats btn-black btn btn-block" style="max-height: 32px">Refresh</button>
                                     </div>
-                                 </div>
-                                 <div class="row" style="border-top: 1px solid #f4f4f4; padding-top: 10px; margin-top: 10px">
-                                    <div class="col-md-4">
-                                        Generic report date scope:    
-                                    </div>
-                                    <div class="col-md-8 text-right">
-                                        <div class="pull-right time-periods">
-                                            <input type="hidden" name="date_period" value="today">
-                                            <button data-period="today" class="btn btn-secondary change-period active" type="button">Today</button>
-                                            <button data-period="yesterday" class="btn btn-secondary change-period" type="button">Yesterday</button>
-                                            <button data-period="last7" class="btn btn-secondary change-period" type="button">Last 7 Days</button>
-                                            <button data-period="last30" class="btn btn-secondary change-period" type="button">This Month</button>
-                                            <button data-period="custom" class="btn btn-secondary change-period" id="show-daterange" type="button">
-                                                Custom Range
-                                                <span class="caret"></span>
-                                            </button> 
-                                        </div> 
-                                    </div> 
-                                 </div>
-                                 <div class="date-range-area" id="date-range-area">
-                                    <div>
-                                         <div class="col-md-5 left">
-                                            <input autocomplete="off" type="text" class="form-control" name="start_date" id="start_date"> 
-                                         </div>
-                                         <div class="col-md-1">
-                                            to
-                                         </div>
-                                         <div class="col-md-5 right"> 
-                                            <input autocomplete="off" type="text" class="form-control" name="send_date" id="end_date">
-                                         </div>
-                                         <div class="col-md-2"> 
-                                            <button type="button" class="filter-dates btn btn-block btn-primary">Filter</button>
-                                         </div>
-                                    </div>
-                                </div>
+                                 </div> 
                             </div>
                         </div>  
                     </div>
