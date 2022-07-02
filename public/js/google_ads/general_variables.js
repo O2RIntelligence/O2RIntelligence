2a4e71f19fe4f44c210cc106650ba7f8774959a1
Mohammed Nayeem <chiefnayeem@gmail.com>
2022-07-02 20:02:25 +0600

    O2R-124 dropdown context menu

--- public/js/google_ads/general_variables.js
+++ public/js/google_ads/general_variables.js
@@ -62,6 +62,22 @@ class GeneralVariables extends GoogleAdsManager {
         item?.official_dollar ?? 0,
         item?.blue_dollar ?? 0,
         Number(item?.plus_m_discount ?? 0).toFixed(2),
+        `<div class="grid-dropdown-actions dropdown">
+          <a href="#" style="padding: 0 10px;" class="dropdown-toggle" data-toggle="dropdown">
+            <i class="fa fa-ellipsis-v"></i>
+          </a>
+          <ul class="dropdown-menu" style="min-width: 70px !important;box-shadow: 0 2px 3px 0 rgba(0,0,0,.2);border-radius:0;left: -65px;top: 5px;">
+            <li>
+              <a href="http://127.0.0.1:8000/admin/auth/users/14/edit">Edit</a>
+            </li>
+            <li>
+              <a href="http://127.0.0.1:8000/admin/auth/users/14">Show</a>
+            </li>
+            <li>
+              <a data-_key="14" href="javascript:void(0);" class="grid-row-action-62b58e69c638a2961">Delete</a>
+            </li>
+          </ul>
+        </div>`
       ]);
     }
     table.draw();
