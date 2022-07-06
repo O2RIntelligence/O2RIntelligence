9655ccd288fe0bf8b29cc10ac8020972e716c1ff
saeed-bin <saeed@blubirdinteractive.com>
2022-07-06 20:30:47 +0600

    O2R-145 added table dropdown design

--- resources/views/googleAds/financial-report/index.blade.php
+++ resources/views/googleAds/financial-report/index.blade.php
@@ -100,23 +100,7 @@
         <section class="content"> 
             <div class="row">
                 <div class="col-md-12">
-                    <div class="box grid-box">
-                        <div class="box-header with-border"> 
-                              <div class="btn-group pull-right" style="margin-right: 10px">
-                                  <a href="http://127.0.0.1:8000/admin/auth/users?_export_=all" target="_blank" class="btn btn-sm btn-twitter" title="Export"><i class="fa fa-download"></i><span class="hidden-xs"> Export</span></a>
-                                  <button type="button" class="btn btn-sm btn-twitter dropdown-toggle" data-toggle="dropdown">
-                                      <span class="caret"></span>
-                                      <span class="sr-only">Toggle Dropdown</span>
-                                  </button>
-                                  <ul class="dropdown-menu" role="menu">
-                                      <li><a href="http://127.0.0.1:8000/admin/auth/users?_export_=all" target="_blank">All</a></li>
-                                      <li><a href="http://127.0.0.1:8000/admin/auth/users?_export_=page%3A1" target="_blank">Current page</a></li>
-                                      <li><a href="http://127.0.0.1:8000/admin/auth/users?_export_=selected%3A__rows__" target="_blank" class="export-selected">Selected rows</a></li>
-                                  </ul>
-                              </div>  
-                        </div>
-
-                         
+                    <div class="box grid-box"> 
                         <table class="table table-hover grid-table" id="financialTable">
                             <thead>
                               <tr> 
